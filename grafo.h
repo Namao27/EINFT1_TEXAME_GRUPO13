090550b0aa89df46e09142f7243a5fa67306e9a7
elielson(badeli) <elielsonseabra2006@gmail.com>
2026-06-26 11:58:21 +0100

    Integração do grafo com o sistema de palavras concluída

--- grafo.h
+++ grafo.h
@@ -1,8 +1,26 @@
 #ifndef GRAFO_H
 #define GRAFO_H
 
-void adicionarRelacao();
-void mostrarRelacoes();
-void removerRelacao();
+#include "palavras.h"
+
+// nó da lista de adjacência
+typedef struct No {
+    int destino;
+    struct No *prox;
+} No;
+
+// grafo
+typedef struct {
+    int numVertices;
+    No *lista[1000];
+} Grafo;
+
+// funções
+void inicializarGrafo(Grafo *g, int vertices);
+void adicionarRelacao(Grafo *g, int origem, int destino);
+void mostrarRelacoes(Grafo *g, int indice, Palavra palavras[], int numPalavras);
+void removerRelacao(Grafo *g, int origem, int destino);
+
+int indiceValido(int i, int max);
 
 #endif
